7661b22d3875357fc29cb9300882d0e5181d9037
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-08-08 01:24:33 +0800

    add active link when page scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,6 +67,29 @@ const toggleItem = (item) => {
 };
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector(`.nav__menu a[href*=${sectionId}]`)
+                .classList.add("active-link");
+        } else {
+            document
+                .querySelector(`.nav__menu a[href*=${sectionId}]`)
+                .classList.remove("active-link");
+        }
+    });
+}
+
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
 
